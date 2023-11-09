672c1876fd40b60dcc6174b8662c60f6637caa46
Brian Marete <bmarete10@gmail.com>
2023-11-09 14:47:41 +0300

    Make color of status dynamic

--- app/(components)/StatusBadge.jsx
+++ app/(components)/StatusBadge.jsx
@@ -1,7 +1,17 @@
-const StatusBadge = () => {
+const StatusBadge = ({ status }) => {
+  const statusColors = {
+    "not started": "bg-red-200",
+    "in progress": "bg-yellow-200",
+    completed: "bg-green-200",
+  };
+
   return (
-    <span className="inline-block h-6 rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-200">
-      done
+    <span
+      className={`inline-block h-6 rounded-full px-2 py-1 text-xs font-semibold text-gray-700 ${
+        statusColors[status] || "bg-slate-400"
+      }`}
+    >
+      {status}
     </span>
   );
 };
