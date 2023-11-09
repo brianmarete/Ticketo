1245d220851f441bdd280f7bd6df64629547c15f
Brian Marete <bmarete10@gmail.com>
2023-11-09 14:48:29 +0300

    remove placeholders from ticket card

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -3,23 +3,23 @@ import PriorityDisplay from "./PriorityDisplay";
 import ProgressBar from "./ProgressBar";
 import StatusBadge from "./StatusBadge";
 
-const TicketCard = () => {
+const TicketCard = ({ ticket }) => {
   return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <div className="flex justify-between mb-3">
-        <PriorityDisplay />
+        <PriorityDisplay priority={ticket.priority} />
         <DeleteButton />
       </div>
-      <h4>Ticket title</h4>
+      <h4>{ticket.title}</h4>
       <hr className="h-px border-0 bg-page mb-2" />
-      <p className="whitespace-pre-wrap">Ticket description</p>
+      <p className="whitespace-pre-wrap">{ticket.description}</p>
       <div className="flex-grow"></div>
       <div className="flex justify-between mt-2">
         <div className="flex flex-col">
-          <p className="text-xs my-1">8/11/23 10:43AM</p>
-          <ProgressBar />
+          <p className="text-xs my-1">{ticket.createdAt}</p>
+          <ProgressBar progress={ticket.progress} />
         </div>
-        <StatusBadge />
+        <StatusBadge status={ticket.status} />
       </div>
     </div>
   );
