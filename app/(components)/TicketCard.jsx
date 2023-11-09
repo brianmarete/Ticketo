7472e353534de53ac6d937a236e92b7dc3436152
Brian Marete <bmarete10@gmail.com>
2023-11-09 14:53:10 +0300

    Make time stamp look better

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -4,6 +4,18 @@ import ProgressBar from "./ProgressBar";
 import StatusBadge from "./StatusBadge";
 
 const TicketCard = ({ ticket }) => {
+  const formatTimestamp = (timestamp) => {
+    const date = new Date(timestamp);
+    return date.toLocaleDateString("en-UK", {
+      hour: "2-digit",
+      minute: "numeric",
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+      hour12: true,
+    });
+  };
+
   return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <div className="flex justify-between mb-3">
@@ -16,7 +28,7 @@ const TicketCard = ({ ticket }) => {
       <div className="flex-grow"></div>
       <div className="flex justify-between mt-2">
         <div className="flex flex-col">
-          <p className="text-xs my-1">{ticket.createdAt}</p>
+          <p className="text-xs my-1">{formatTimestamp(ticket.createdAt)}</p>
           <ProgressBar progress={ticket.progress} />
         </div>
         <StatusBadge status={ticket.status} />
